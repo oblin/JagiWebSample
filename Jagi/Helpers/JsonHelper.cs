56584e9216ea4964fca91be9f0bb63f402bb5011
yiming lin <yiming.lin@outlook.com>
2015-10-01 23:28:12 +0800

    建立 JsonHelper 提供 object 轉換 json string 與 Expression 轉成 camel case
    Related Work Items: #453, #459

---
+++ Jagi/Helpers/JsonHelper.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Jagi.Helpers
+{
+    public static class JsonHelper
+    {
+        /// <summary>
+        /// 將物件轉換成 Json 表達式； ex: {"number":11,"text":"Sample Text","isChinese":false,"startDate":"0001-01-01T00:00:00","endDate":null,"nullableInt":null}
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">任意物件</param>
+        /// <param name="includeNull">是否將 null 轉入預設 true，如果不要將 null 轉入，則會沒有 null 值的屬性</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T obj, bool includeNull = true)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new JsonConverter[] { new StringEnumConverter() },
+                NullValueHandling = includeNull ? NullValueHandling.Include : NullValueHandling.Ignore
+            };
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+
+        /// <summary>
+        /// Turns x => x.SomeProperty.SomeValue into "someProperty.someValue"
+        /// 請注意前面的 x. 會被刪除
+        /// 這裡主要的用途在於提供 angular ngModel 與 razor Model 的命名對應
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TProp"></typeparam>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static string ToCamelCaseName<TModel, TProp>(
+            this Expression<Func<TModel, TProp>> property)
+        {
+            //Turns x => x.SomeProperty.SomeValue into "SomeProperty.SomeValue" 
+            var pascalCaseName = ExpressionHelper.GetExpressionText(property);
+
+            //Turns "SomeProperty.SomeValue" into "someProperty.someValue"
+            var camelCaseName = ConvertFullNameToCamelCase(pascalCaseName);
+            return camelCaseName;
+        }
+
+        //Converts expressions of the form Some.PropertyName to some.propertyName
+        private static string ConvertFullNameToCamelCase(string pascalCaseName)
+        {
+            var parts = pascalCaseName.Split('.')
+                .Select(ConvertToCamelCase);
+
+            return string.Join(".", parts);
+        }
+
+        //Borrowed from JSON.NET. Turns a single name into camel case.
+        private static string ConvertToCamelCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+            if (!char.IsUpper(s[0]))
+                return s;
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                bool hasNext = (i + 1 < chars.Length);
+                if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+                    break;
+                chars[i] = char.ToLower(chars[i], CultureInfo.InvariantCulture);
+            }
+            return new string(chars);
+        }
+    }
+}
---
+++ UnitTestJagi/JsonHelperTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jagi.Helpers;
+using Newtonsoft.Json;
+using System.Linq.Expressions;
+
+namespace UnitTestJagi
+{
+    /// <summary>
+    /// Summary description for JsonHelperTest
+    /// </summary>
+    [TestClass]
+    public class JsonHelperTest
+    {
+        [TestMethod]
+        public void Test_Sample_Object_ToJson()
+        {
+            Sample sample = new Sample { Number = 11, Text = "Sample Text" };
+            string jsonString = sample.ToJson();
+
+            bool isCamelText = jsonString.IndexOf("text", 0, StringComparison.Ordinal) > 0;
+            Assert.IsTrue(isCamelText);
+
+            bool isPascalNumber = jsonString.IndexOf("Number", 0, StringComparison.Ordinal) > 0;
+            Assert.IsFalse(isPascalNumber);
+        }
+
+        [TestMethod]
+        public void Test_Deserialized_Json_To_Dynamic()
+        {
+            Sample sample = new Sample { Number = 11, Text = "Sample Text" };
+            string jsonString = sample.ToJson();
+
+            var resultObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            Assert.AreEqual(11, (int)resultObject.number);
+            Assert.AreEqual("Sample Text", (string)resultObject.text);
+
+            Assert.AreEqual("11", (string)resultObject["number"]);
+        }
+
+        [TestMethod]
+        public void Test_Deserialized_Json_To_Dynamic_Without_Null()
+        {
+            SampleCopy2 sample = new SampleCopy2 { Number = 11, Text = "Sample Text" };
+            string jsonString = sample.ToJson(false);
+
+            var resultObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            Assert.AreEqual(11, (int)resultObject.number);
+            Assert.IsNull(resultObject.startDate);
+
+            sample = new SampleCopy2 { Number = 11, Text = "Sample Text", StartDate = DateTime.Now };
+            jsonString = sample.ToJson(false);
+
+            resultObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            Assert.AreEqual(11, (int)resultObject.number);
+            Assert.IsNotNull(resultObject.startDate);
+
+            Assert.IsInstanceOfType((DateTime)resultObject.startDate, typeof(DateTime));
+        }
+
+        [TestMethod]
+        public void Test_Expression_ToCamelCase_Number()
+        {
+            Expression<Func<Sample, int>> expr = a => a.Number;
+
+            string result = expr.ToCamelCaseName();
+
+            Assert.AreEqual("number", result);
+        }
+
+        [TestMethod]
+        public void Test_Expression_ToCamelCase_ComplexProp_Number()
+        {
+            Expression<Func<SampleCopy2, int>> expr = a => a.ComplexProp.Number;
+
+            string result = expr.ToCamelCaseName();
+
+            Assert.AreEqual("complexProp.number", result);
+        }
+    }
+}
