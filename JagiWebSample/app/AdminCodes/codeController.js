9ffcbd7fe2e84a84f611438f59af42799d0b65d5
yiming lin <yiming.lin@outlook.com>
2015-10-13 22:53:43 +0800

    使用 JsonFor 建立 angular Constant，再注入 Controller 中
    Related Work Items: #463

--- JagiWebSample/app/AdminCodes/codeController.js
+++ JagiWebSample/app/AdminCodes/codeController.js
@@ -1,7 +1,11 @@
 ﻿(function () {
     'use strict';
 
-    app.controller('code', ['$scope', codeController]);
+    window.app.controller('codeController', codeController);
+    codeController.$inject = ['model'];
 
-    function codeController($scope) { }
+    function codeController(model) {
+        var vm = this;
+        vm.codes = model;
+    }
 })();
