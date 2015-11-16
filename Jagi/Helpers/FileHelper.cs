2a3340e31ac2752fcb4646e5848078b83d973369
yiming <oblin@msn.com>
2015-11-16 09:37:18 +0800

    加入 FileHelper 處理檔案管理

---
+++ Jagi/Helpers/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Jagi.Helpers
+{
+    public class FileHelper
+    {
+        public static void CopyFile(string file, string template)
+        {
+            if (File.Exists(file))
+            {
+                // 刪除判斷建立時間之功能
+                //DateTime createTime = File.GetCreationTime(file);
+                //TimeSpan substract = DateTime.Now.Subtract(createTime);
+                //if (substract.Minutes < 30)
+                //    return false;
+
+                File.Delete(file);
+            }
+
+            File.Copy(template, file);
+            File.SetLastWriteTime(file, DateTime.Now);
+        }
+    }
+}
